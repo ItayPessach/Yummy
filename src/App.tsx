66f7e3c94615e362c16dc308ef9de5303626fd10
ItayPessach <etaypess@gmail.com>
2024-01-06 22:56:42 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div style={{display: 'flex', justifyContent: 'center'}}>
+       <span style={{textAlign: 'center', fontSize: '40px'}}>Initial Commit Of The app</span>
+    </div>
+)}
+
+export default App
